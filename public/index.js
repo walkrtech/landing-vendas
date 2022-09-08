b4b12b026bbd2ad2dcfdaf226c161ee8658caaed
Aenã <aenamartinelli@gmail.com>
2022-09-08 09:55:45 -0300

    new js script to aux in animations

---
+++ public/index.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', () => {
+    document.querySelectorAll('.reveal').forEach(reveal => {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveal.getBoundingClientRect().top;
+
+        if(revealTop < windowHeight - 200){
+            reveal.classList.add('slidein')
+        }else{
+            reveal.classList.remove('slidein')
+        }
+    })
+})
+
+function display(module) {
+    
+    document.querySelectorAll(`.module-${module}`).forEach(element => {
+        if(element.classList.contains("mobile")){
+            element.classList.remove("mobile")
+            document.getElementById(`arrow-${module}`).setAttribute("src", "https://landing-vendas.s3.amazonaws.com/icons/arrow-right.svg")
+        }else {
+            document.getElementById(`arrow-${module}`).setAttribute("src", "https://landing-vendas.s3.amazonaws.com/icons/arrow-down.svg")
+            element.classList.add("mobile")
+        }
+    });
+}
